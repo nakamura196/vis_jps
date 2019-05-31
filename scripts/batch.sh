e429c02ca8544dbc0ab9dd683ab6fc34b9bd323f
nakamura196 <na.kamura.1263@gmail.com>
2019-06-01 08:15:14 +0900

    add images

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -1,7 +1,7 @@
 python get_captures.py ../data/src/pd_items.json ../data/captures.json;
 python get_collections.py ../data/src/pd_items.json ../data/collections.json ../data/item_collections.json;
-//python get_color_data.py ../data/captures.json ../img/items/ ../data/item_hsl.json 3;
-//python get_colors.py ../data/item_hsl.json ../data/colors.json ../data/item_colors.json;
+python get_color_data.py ../data/captures.json ../img/items/ ../data/item_hsl.json 3;
+python get_colors.py ../data/item_hsl.json ../data/colors.json ../data/item_colors.json;
 python get_dates.py ../data/src/pd_items.json ../data/centuries.json ../data/item_centuries.json century;
 python get_genres.py ../data/src/pd_items.json ../data/genres.json ../data/item_genres.json;
 python stitch_images.py ../data/ ../img/items/ ../img/ 200 5 5 default 50 20 3 80000
